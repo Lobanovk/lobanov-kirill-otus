79735a721864a361689a1a9cfad72a0e7962bb5e
Lobanov Kirill <lobanovk@list.ru>
2021-10-18 14:07:27 +0300

    hm3: added test

--- hw3/getPath.test.js
+++ hw3/getPath.test.js
@@ -1,7 +1,18 @@
 const { getPath } = require('./getPath');
 
 test("should return uniq selector", () => {
-  document.body
-
-  expect(true).toBe(true);
+  document.body.innerHTML = `
+    <div class="first">
+    <div class="content">
+      <div class="text">text1</div>
+    </div>
+  </div>
+  <div class="first">
+    <div class="content">  
+    </div>
+  </div>
+  `;
+  const element = document.querySelector("div.first:nth-child(2) div.content")
+  expect(document.querySelectorAll(getPath(element)).length).toBe(1);
+  expect(element).toMatchObject(document.querySelectorAll(getPath(element)[0]));
 });
